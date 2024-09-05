631c40be18f23cbf803fa5454ae683df7ea06a73
Andrius <andrius2384@gmail.com>
2024-09-05 18:49:51 +0300

    Task done.

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,24 +1,59 @@
 package com.example.lab2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Spinner countSpinner = findViewById(R.id.spSelector);
+        ArrayList<String> options = new ArrayList<>();
+        options.add("Words");
+        options.add("Symbols");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.spinner_item, R.id.spinner_item_text, options);
+        adapter.setDropDownViewResource(R.layout.spinner_item);
+
+        countSpinner.setAdapter(adapter);
+
+        countSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id  ) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this,"Selected: "+ selectedItem, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
         });
-    }
+        TextView countView = findViewById(R.id.countView);
+        Button CountBtn = findViewById(R.id.btnCount);
+        EditText edUserInput = findViewById(R.id.edUserInputText);
+        CountBtn.setOnClickListener(v-> {
+            String selectedOption = (String) countSpinner.getSelectedItem();
+            String inputText = edUserInput.getText().toString();
+            int count = Functions.countText(MainActivity.this, selectedOption, inputText);
+            if(count > 0)
+                countView.setText("  Count: " + count);
+        });
+        }
 }
